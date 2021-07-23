5603620a247fa9b66ab1cfe3a758ecaab3d97728
Matheus Rufino Cruz <mrcruz1@stefanini.com>
2021-07-23 01:18:12 -0300

    feature: (feat/frontEnd-1): Estrutura inicial do front-end com algumas paginas de exemplo

---
+++ src/app/produtos/produto.ts
@@ -0,0 +1,8 @@
+export class Produto {
+   public id: string;
+   public nome: string;
+   public valor: string;
+   public promocao: boolean;
+   public  valorPromo: string;
+   public  imagem: string;    
+}
